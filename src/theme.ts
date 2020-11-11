2bcc8a90ceb717b5664bb10fbb85c2bf4097031e
chrisjgf <chrisjgf@gmail.com>
2020-11-11 13:34:50 +0100

    Add lightblue

--- src/theme.ts
+++ src/theme.ts
@@ -3,6 +3,7 @@ export enum Colors {
   goldTransparent = 'rgb(255,179,52, 0.2)',
   green = 'rgb(82,204,147)',
   greenTransparent = 'rgba(82,204,147, 0.2)',
+  lightBlue = 'rgb(20,160,255)',
   blue = 'rgb(23,110,222)',
   blueTransparent = 'rgba(0,92,222,0.2)',
   red = 'rgb(202,0,27)',
