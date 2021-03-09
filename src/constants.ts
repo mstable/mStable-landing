9108e3d939e2039ffc2296781b0d8a604d5dacac
James Lefrere <james.lefrere@mstable.org>
2021-03-09 20:44:22 +0100

    Bye

--- src/constants.ts
+++
@@ -1,2 +0,0 @@
-export const STATS_API_ENDPOINT =
-  'https://simple-stats-api-dot-mstable.ts.r.appspot.com/'
--- src/strings.ts
+++
@@ -1,6 +0,0 @@
-export const metadata = { 
-  title:
-    'mStable – Autonomous and non-custodial stablecoin infrastructure',
-  description:
-    'mStable unites stablecoins, lending and swapping into one standard.'
-}
