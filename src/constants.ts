7cc16415ac673c732b28d458aea0bb44dfe6ff7c
James Lefrere <james.lefrere@mstable.org>
2021-03-10 01:16:10 +0100

    Temp: point to staging stats

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,3 @@
-export const STATS_API_ENDPOINT =
-  'https://simple-stats-api-dot-mstable.ts.r.appspot.com/'
+// export const STATS_API_ENDPOINT = 'https://simple-stats-api-dot-mstable.ts.r.appspot.com/'
+export const STATS_API_ENDPOINT = 'https://simple-stats-api-dot-mstable-staging.ts.r.appspot.com/'
+// export const STATS_API_ENDPOINT = 'http://localhost:8080'
